163c4ed5cf1807b2d36585e5422bc266dc5eecb2
Daniel Wiklund <daniel@happyrabbit.com>
2014-07-13 20:59:53 +0800

    Added Delegator module

---
+++ lib/trans_forms/main_model/delegator.rb
@@ -0,0 +1,10 @@
+module TransForms
+  module MainModel
+    module Delegator
+      # A module that adds functionality to delegate certain
+      # methods to the main model. This is done to make forms
+      # and controllers handle the form model as if it were
+      # the main model itself
+    end
+  end
+end
