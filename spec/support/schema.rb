5299b6bff6a9839d58ca72bdc503ae3aedd7018c
Daniel Wiklund <daniel@happyrabbit.com>
2019-11-30 08:34:25 +0800

    Use schema define instead of migration for tests

--- spec/support/schema.rb
+++ spec/support/schema.rb
@@ -6,20 +6,18 @@ ActiveRecord::Base.establish_connection({
 })
 
 # prepare test data
-class CreateTestSchema < ActiveRecord::Migration
-  def change
-    create_table "users", :force => true do |t|
-      t.string "name"
-      t.integer "age"
-      t.datetime "created_at",    :null => false
-      t.datetime "updated_at",    :null => false
-    end
+ActiveRecord::Schema.define do
+  self.verbose = false
 
-    create_table "phone_numbers", :force => true do |t|
-      t.integer "user_id"
-      t.string "number"
-    end
+  create_table "users", :force => true do |t|
+    t.string "name"
+    t.integer "age"
+    t.datetime "created_at",    :null => false
+    t.datetime "updated_at",    :null => false
+  end
+
+  create_table "phone_numbers", :force => true do |t|
+    t.integer "user_id"
+    t.string "number"
   end
 end
-
-CreateTestSchema.migrate(:up)
